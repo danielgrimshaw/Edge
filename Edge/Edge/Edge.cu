bcde8c6de384effc7087ba08ed6ea33b976aaa7f
Daniel Grimshaw <danielgrimshaw42@gmail.com>
2016-02-17 20:02:56 -0800

    Complete preProcess algorithm

--- Edge/Edge/Edge.cu
+++ Edge/Edge/Edge.cu
@@ -8,8 +8,8 @@ int _tmain(int argc, _TCHAR * argv[]) {
 	using namespace std;
 	string inputFile;
 	string outputFile;
-	unsigned char *h_originalImage, *d_originalImage;
-	unsigned char *h_edgeImage, *d_edgeImage;
+	uchar4 *h_originalImage, *d_originalImage;
+	uchar4 *h_edgeImage, *d_edgeImage;
 
 	if (argc == 3) {
 		inputFile = string((char *)argv[1]);
@@ -25,10 +25,13 @@ int _tmain(int argc, _TCHAR * argv[]) {
 	return 0;
 }
 
-void preProcess(unsigned char **inputImage, unsigned char **edgeImage,
-	unsigned char **d_originalImage, unsigned char **d_edgeImage,
+void preProcess(uchar4 **inputImage, uchar4 **edgeImage,
+	uchar4 **d_originalImage, uchar4 **d_edgeImage,
 	const std::string & filename) {
 	cudaFree(0);
+
+	cv::Mat imageOrig;
+	cv::Mat imageEdge;
 	
 	cv::Mat image;
 	image = cv::imread(filename.c_str(), CV_LOAD_IMAGE_COLOR);
@@ -37,7 +40,27 @@ void preProcess(unsigned char **inputImage, unsigned char **edgeImage,
 		exit(1);
 	}
 	
-	cv::cvtColor(image, imageRGBA, CV_BGR2RGBA);
+	cv::cvtColor(image, imageOrig, CV_BGR2RGBA);
 	
+	// allocate memory for the output
+	imageEdge.create(image.rows, image.cols, CV_8UC4);
+
+	if (!imageOrig.isContinuous() || !imageEdge.isContinuous()) {
+		std::cerr << "Images aren't continuous!!! Aborting." << std::endl;
+		exit(1);
+	}
 	
+	*inputImage = (uchar4 *)imageOrig.ptr<unsigned char>(0);
+	*edgeImage = (uchar4 *)imageEdge.ptr<unsigned char>(0);
+
+	const size_t numPixels = imageOrig.rows * imageOrig.cols;
+
+	// allocate device memory
+	cudaMalloc(d_originalImage, sizeof(uchar4) * numPixels);
+	cudaMalloc(d_edgeImage, sizeof(uchar4) * numPixels);
+	cudaMemset(*d_edgeImage, 0, sizeof(uchar4) * numPixels);
+
+	// copy inputs to GPU
+	cudaMemcpy(*d_originalImage, *inputImage, sizeof(uchar4) * numPixels, cudaMemcpyHostToDevice);
 }
+	
